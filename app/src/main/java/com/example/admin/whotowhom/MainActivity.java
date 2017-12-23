5ebb6f1a6c540aedec83198840e88d42a222a3e4
Admin <ladani.smit49@gmail.com>
2017-12-23 11:08:06 +0530

    Two Activity

--- app/src/main/java/com/example/admin/whotowhom/MainActivity.java
+++ app/src/main/java/com/example/admin/whotowhom/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.admin.whotowhom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText text1;
+    Button btn1;
+    String s1;
+    int no;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text1=(EditText)findViewById(R.id.editText);
+        btn1=(Button)findViewById(R.id.button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btn1.setOnClickListener(v->{
+            s1=text1.getText().toString();
+            no=Integer.parseInt(s1);
+            Intent in1=new Intent(MainActivity.this,SecondActivity.class);
+            Bundle b=new Bundle();
+            b.putInt("Number",no);
+            in1.putExtra("myBundle",b);
+            startActivity(in1);
+        });
     }
 }
